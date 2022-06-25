826d1df9a6c475b8905a80bdc6760e00ba4bd659
lauradillonar <laura.dillon.ar@gmail.com>
2022-06-25 18:53:37 -0300

    initial commit

---
+++ src/main/java/com/confluenciacreativa/empleadosbackend/EmpleadosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.confluenciacreativa.empleadosbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpleadosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpleadosBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/confluenciacreativa/empleadosbackend/EmpleadosBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.confluenciacreativa.empleadosbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpleadosBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
